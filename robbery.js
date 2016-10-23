da0df9407d8f7677a6bb0e13eddb7c25a8ae2601
Nikolai Bravilov <bravilov@mail.ru>
2016-10-23 19:03:29 +0500

    Создание временной шкалы

--- robbery.js
+++ robbery.js
@@ -6,7 +6,19 @@
  */
 exports.isStar = true;
 
+var timePattern = /^(?:([a-zа-я]{2}) )?(\d\d):(\d\d)\+(\d+)$/i;
+var dayByName = { undefined: 0, 'ВС': 1, 'ПН': 2, 'ВТ': 3, 'СР': 4, 'ЧТ': 5 };
+
+var DEFAULT_YEAR = 1970;
+var DEFAULT_MONTH = 0;
+
+var bankTimeZone = 0;
+
 /**
+ * Алгоритм получения подходящего времени следующий: сначала выстраиваем все времена участников и
+ * банка во временной шкале с идентификатором того, чьё это время. После этого определяем
+ * пересечение всех 4 множеств отрезков, получая множество подходящих для ограбления промежутков
+ * времени.
  * @param {Object} schedule – Расписание Банды
  * @param {Number} duration - Время на ограбление в минутах
  * @param {Object} workingHours – Время работы банка
@@ -15,7 +27,15 @@ exports.isStar = true;
  * @returns {Object}
  */
 exports.getAppropriateMoment = function (schedule, duration, workingHours) {
-    console.info(schedule, duration, workingHours);
+    bankTimeZone = timePattern.exec(workingHours.from)[4];
+
+    var timeLine = [];
+    addBankWorkingTimeToTimeLine(timeLine, workingHours);
+    addGangAppropriateTime(timeLine, schedule);
+
+    timeLine.sort(function (a, b) {
+        return a.timestamp - b.timestamp;
+    });
 
     return {
 
@@ -48,3 +68,62 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
         }
     };
 };
+
+/**
+ * @param {Array} timeLine
+ * @param {object} workingHours
+ */
+function addBankWorkingTimeToTimeLine(timeLine, workingHours) {
+
+    var keys = Object.keys(dayByName);
+    for (var i = 2; i < keys.length - 1; i++) {
+        timeLine.push(
+            {
+                timestamp: parseTime(keys[i] + ' ' + workingHours.from),
+                identifier: 0
+            },
+            {
+                timestamp: parseTime(keys[i] + ' ' + workingHours.to),
+                identifier: 0
+            }
+        );
+    }
+}
+
+/**
+ * @param {Array} timeLine
+ * @param {object} schedule
+ */
+function addGangAppropriateTime(timeLine, schedule) {
+    Object.keys(schedule).forEach(function (key, index) {
+        schedule[key].forEach(function (element) {
+            timeLine.push(
+                {
+                    timestamp: parseTime(element.from),
+                    identifier: index + 1
+                },
+                {
+                    timestamp: parseTime(element.to),
+                    identifier: index + 1
+                }
+            );
+        });
+    });
+}
+
+/**
+ * @param {string} time
+ * @returns {int} - timestamp
+ */
+function parseTime(time) {
+    var parsedTime = timePattern.exec(time);
+
+    return Date.UTC(
+        DEFAULT_YEAR,
+        DEFAULT_MONTH,
+        Number(dayByName[parsedTime[1]]) - 1,
+        // Приводим к одной временной зоне (банковской); +24 чтобы не было отриц. часов
+        Number(parsedTime[2]) - Number(parsedTime[4]) + Number(bankTimeZone) + 24,
+        Number(parsedTime[3])
+    );
+}
